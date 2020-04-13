62d58dee7439d03794dd2e212b8daa074df527a0
Leonardo Costa <leo.costa.programming@gmail.com>
2020-04-13 15:52:39 +0200

    changed setImages & setIsLoading state + created components folder and ImageCard.js file

--- src/App.js
+++ src/App.js
@@ -8,53 +8,15 @@ function App() {
   useEffect(() => {
     fetch(`https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`)
       .then(res => res.json())
-      .then(data => console.log(data))
+      .then(data => {
+        setImages(data.hits);
+        setIsLoading(false);
+      })
       .catch(err => console.log(err)); 
   }, []);
 
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg">
-      <img src="https://source.unsplash.com/random" alt=""
-      className="w-full" />
-      <div className="px-6 py-4">
-        <div className="font-bold text-blue-500 text-xl mb-2">
-          Photo Taken By Unknown
-        </div>
-        
-        <ul>
-          <li>
-            <strong>Views: </strong>
-            4000
-          </li>
 
-          <li>
-            <strong>Downloads: </strong>
-            100
-          </li>
-
-          <li>
-            <strong>Likes: </strong>
-            500
-          </li>
-
-        </ul>
-      </div>
-
-      <div className="px-6 py-4">
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-          #Tag 1
-        </span>
-
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-          #Tag 2
-        </span>
-
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-          #Tag 3
-        </span>
-      </div>
-
-    </div>
   );
 }
 
---
+++ src/components/ImageCard.js
@@ -0,0 +1,50 @@
+import React from 'react'
+
+const ImageCard = () => {
+  return (
+    <div className="max-w-sm rounded overflow-hidden shadow-lg">
+    <img src="https://source.unsplash.com/random" alt=""
+    className="w-full" />
+    <div className="px-6 py-4">
+      <div className="font-bold text-blue-500 text-xl mb-2">
+        Photo Taken By Unknown
+      </div>
+      
+      <ul>
+        <li>
+          <strong>Views: </strong>
+          4000
+        </li>
+
+        <li>
+          <strong>Downloads: </strong>
+          100
+        </li>
+
+        <li>
+          <strong>Likes: </strong>
+          500
+        </li>
+
+      </ul>
+    </div>
+
+    <div className="px-6 py-4">
+      <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+        #Tag 1
+      </span>
+
+      <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+        #Tag 2
+      </span>
+
+      <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+        #Tag 3
+      </span>
+    </div>
+
+  </div>
+  )
+}
+
+export default ImageCard;
